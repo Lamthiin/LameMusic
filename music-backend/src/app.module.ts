ca1d18a459e630b3792201213bf03265d40d2df2
Briannthd <dotanhung0505@gmail.com>
2025-11-25 18:00:11 +0700

    TanHung

--- music-backend/src/app.module.ts
+++ music-backend/src/app.module.ts
@@ -30,7 +30,7 @@ import { AdminModule } from './admin/admin.module';
       host: 'localhost',
       port: 3306,
       username: 'root', // Đảm bảo đúng username
-      password: 'root',     // Đảm bảo đúng password
+      password: '123456',     // Đảm bảo đúng password
       database: 'musicdb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false, 
