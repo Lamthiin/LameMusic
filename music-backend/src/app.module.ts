18c628f0b899df43b2d7f4660d6238a4ca5a4e7c
Briannthd <dotanhung0505@gmail.com>
2025-11-27 20:20:37 +0700

    app,module.ts

--- music-backend/src/app.module.ts
+++ music-backend/src/app.module.ts
@@ -29,8 +29,8 @@ import { AdminModule } from './admin/admin.module';
       type: 'mysql', // <-- DÒNG NÀY SẼ SỬA LỖI CỦA BẠN
       host: 'localhost',
       port: 3306,
-      username: 'hiin', // Đảm bảo đúng username
-      password: '1234',     // Đảm bảo đúng password
+      username: 'root', // Đảm bảo đúng username
+      password: '123456',     // Đảm bảo đúng password
       database: 'musicdb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false, 
