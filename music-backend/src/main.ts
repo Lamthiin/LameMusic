54cc01e6e463c9ed62942116c4b4c7355053196a
LamThuyenNguyen <nguyenngoclamthuyen0977495533@gmail.com>
2025-12-29 04:27:49 +0700

    ...

--- music-backend/src/main.ts
+++ music-backend/src/main.ts
@@ -6,26 +6,17 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
 async function bootstrap() {
+  console.log('🔥🔥 CORS VERSION 2 IS RUNNING 🔥🔥');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // ⭐ ENABLE CORS – PHẢI ĐẶT TRƯỚC app.listen
   app.enableCors({
     origin: [
       'https://lamemusic-1.onrender.com',
       'http://localhost:5173',
     ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
