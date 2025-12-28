4f8293548da779eac4685f3f69f1c1fbb6e3ab76
LamThuyenNguyen <nguyenngoclamthuyen0977495533@gmail.com>
2025-12-29 03:51:22 +0700

    xoa api du

--- music-frontend/src/components/user/CreatePlaylistModal.jsx
+++ music-frontend/src/components/user/CreatePlaylistModal.jsx
@@ -4,12 +4,6 @@ import axios from 'axios';
 import './CreatePlaylistModal.css';
 import { api } from "@/utils/api";
 
-const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    import.meta.env.VITE_API_BASE ||
-    'http://localhost:3000',
-});
 
 const CreatePlaylistModal = ({ isOpen, onClose, onPlaylistCreated }) => {
   const token = localStorage.getItem('accessToken');
